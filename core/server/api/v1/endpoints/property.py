d3f86a11c3f1731ac363bcdd259a052a5ad11d68
mdawess <michaelanthony.dawes@mail.utoronto.ca>
2025-07-28 21:11:02 -0400

    Add property management to server

--- core/server/api/v1/endpoints/property.py
+++ core/server/api/v1/endpoints/property.py
@@ -31,6 +31,11 @@ async def list_properties(
     request: Request,
 ) -> List[PropertyResponse]:
     properties = request.app.state.db.get_properties(property_request.user_id)
+    print(f"properties: {properties}")
+    if not properties:
+        return []
+    
+    properties = [PropertyResponse(id=property['id'], name=property['name']) for property in properties]
     return properties
 
 
@@ -41,5 +46,7 @@ async def add_property(
 ) -> PropertyResponse:
     # TODO: This also needs to allow people to set up a pi for the new property
     # Maybe its a + button on the page after going to the property page?
-    property = request.app.state.db.add_property(property_request.user_id)
-    return property
+    success = request.app.state.db.add_property(property_request.user_id)
+    if not success:
+        raise HTTPException(status_code=500, detail="Failed to add property")
+    return PropertyResponse(id=property_request.user_id, name="New Property")
