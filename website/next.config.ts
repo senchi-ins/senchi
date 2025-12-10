b02d1691cf0fb6a6cd79629252794554e5b73ee3
Adam Trotman <adam.m.trotman@gmail.com>
2025-12-10 11:22:15 -0800

    Redirecting senchi.ca (any path) to joinaerium.com

--- website/next.config.ts
+++ website/next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://joinaerium.com',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
